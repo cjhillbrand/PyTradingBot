aceb971c9630b2e3c8a68d912215ecc9e169b32d
Christopher Hillbrand <cjhillbrand@gmail.com>
2020-04-15 11:39:42 -0700

    Polling for Database

--- DBConnection.py
+++ DBConnection.py
@@ -1,4 +1,5 @@
 import psycopg2
+from MiddleWare import MiddleWare
 
 hostname = 'localhost'
 username = 'cj'
@@ -6,10 +7,12 @@ password = 'Legomann23'
 database = 'TradingBot'
 myConnection = psycopg2.connect(host=hostname, user=username, password=password,
                                 dbname=database)
+mw = MiddleWare()
 cur = myConnection.cursor()
-
-cur.execute("INSERT INTO historicaldata (symbol, open_price) VALUES ('TSTE', '10.0')")
-
-myConnection.commit()
+while (True):
+    price = mw.get_crypto_price("spot", 'BTC')
+    cur.execute("INSERT INTO dailycryptodata (symbol, datetime, price) VALUES ('BTC', CURRENT_TIMESTAMP, " +
+                price['Price'] + ")")
+    myConnection.commit()
 
 myConnection.close()
